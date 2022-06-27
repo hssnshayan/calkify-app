096e59bd3f247eee556fd25f06c4cc8d552dfe71
Hossein Shayan <hssnshayan@gmail.com>
2022-06-28 00:03:23 +0430

    feat: :fire: yohoooo, slider is almost done

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,31 +1,41 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
+import * as model from "./model.js";
 import mainView from "./views/mainView.js";
+import { SLIDER_DELAY } from "./config.js";
 
-const init = function () {
-  mainView.removeAfterClickEffect();
-};
-init();
-
-const sliderBtns = document.querySelector("#slider-btns");
-const sliderElements = document.querySelectorAll(".slider-element");
-
-const sliderChange = function (counter, sliders, offset) {
-  const numOfSliders = sliders.length;
-
-  if (counter >= numOfSliders && offset < 0) counter = 0;
-  if (counter === 0 && offset > 0) counter = numOfSliders - 1;
-
-  sliders.forEach((element) => {
-    element.style.transform = `translateX(${counter * offset}px)`;
+const sliderChange = function (sliders, count) {
+  sliders.forEach((element, index) => {
+    element.style.transform = `translateX(-${100 * count}%)`;
   });
 };
 
-sliderBtns.addEventListener("click", (e) => {
-  let offset;
-  if (e.target.classList.contains("btn-prev")) offset = +720;
-  else if (e.target.classList.contains("btn-next")) offset = -720;
-  else return;
-  sliderChange(0, sliderElements, offset)
-});
+const nextSlide = function (slides, numOfSlides) {
+  if (model.state.sliderCounter === numOfSlides - 1)
+    model.state.sliderCounter = 0;
+  else model.state.sliderCounter++;
+  sliderChange(slides, model.state.sliderCounter);
+};
+
+const prevSlide = function (slides, numOfSlides) {
+  if (model.state.sliderCounter === 0)
+    model.state.sliderCounter = numOfSlides - 1;
+  else model.state.sliderCounter--;
+  sliderChange(slides, model.state.sliderCounter);
+};
+
+const autoSlider = function (slides, numOfSlides) {
+  setInterval(() => {
+    nextSlide(slides, numOfSlides);
+  }, SLIDER_DELAY * 1000);
+};
+
+const init = function () {
+  mainView.removeAfterClickEffect();
+  mainView.addBtnSliserHandler(nextSlide, prevSlide);
+  mainView.addKeySliserHandler(nextSlide, prevSlide);
+  mainView.autoSliderHandler(autoSlider);
+};
+
+init();
--- src/js/views/mainView.js
+++ src/js/views/mainView.js
@@ -1,5 +1,8 @@
 class mainView {
   _allButtons = document.querySelectorAll("button");
+  _sliderBtns = document.querySelector("#slider-btns");
+  _sliderElements = document.querySelectorAll(".slider-element");
+  _numOfSlides = this._sliderElements.length;
 
   removeAfterClickEffect() {
     this._allButtons.forEach((btn) => {
@@ -8,6 +11,29 @@ class mainView {
         e.target.blur();
       });
     });
+  }
+
+  addBtnSliserHandler(handlerNext, handlerPrev) {
+    this._sliderBtns.addEventListener("click", (e) => {
+      console.log(this._counter);
+      if (e.target.classList.contains("btn-next"))
+        handlerNext(this._sliderElements, this._numOfSlides);
+      if (e.target.classList.contains("btn-prev"))
+        handlerPrev(this._sliderElements, this._numOfSlides);
+    });
+  }
+
+  addKeySliserHandler(handlerNext, handlerPrev) {
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowRight")
+        handlerNext(this._sliderElements, this._numOfSlides);
+      if (e.key === "ArrowLeft")
+        handlerPrev(this._sliderElements, this._numOfSlides);
+    });
+  }
+  
+  autoSliderHandler(handler) {
+    handler(this._sliderElements, this._numOfSlides);
   }
 }
 
